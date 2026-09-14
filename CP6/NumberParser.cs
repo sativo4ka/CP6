89d4f3959d4d6df08eda24079a0088a48ec66ddc
Administrator <Administrator@MEDVED>
2026-09-14 14:26:59 +0300

    Добавьте файлы проекта.

---
+++ CP6/NumberParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CP6
+{
+    public class NumberParser
+    {
+        public static int SumValidNumbers(List<string> values)
+        {
+            int sum = 0;
+            int processedCount = 0;
+
+            if (values == null)
+                return sum;
+
+            foreach (var value in values)
+            {
+                try
+                {
+                    int number = int.Parse(value);
+                    sum += number;
+                }
+                catch (FormatException) when (string.IsNullOrWhiteSpace(value))
+                {
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"[Предупреждение] Ошибка формата '{value}': {ex.Message}");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"[Ошибка] Переполнение при парсинге '{value}'");
+                    throw;
+                }
+                finally
+                {
+                    processedCount++;
+                    Console.WriteLine($"Обработано элементов: {processedCount}");
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ CP6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CP6 
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var testList = new List<string> { "10", "", "abc", "20" };
+            int result = NumberParser.SumValidNumbers(testList);
+            Console.WriteLine($"Итоговая сумма: {result}");
+        }
+    }
+}
